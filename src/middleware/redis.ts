981ed4d117afe219209d6604dfe64ca0bdf61ee4
Ryoneme2 <worknarair6@gmail.com>
2022-10-21 20:00:50 +0700

    prettier line

--- src/middleware/redis.ts
+++ src/middleware/redis.ts
@@ -5,25 +5,25 @@ import { httpStats } from '../configs/http'
 const cacheCheck = async (req: Request, res: Response, next: NextFunction) => {
   try {
     await connectClient()
+
     const params = Object.values(req.params)
-    console.log(Object.values(req.params))
+
     if (!params) return next()
-    const eachGet = params.map(v => client.get(v))
-    const value = await Promise.all(eachGet)
+
+    const value = await Promise.all(params.map(v => client.get(v)))
+
     if (value.every(v => v === null)) {
       next()
       return
     }
-    console.log('cache hit')
 
-    res.status(200).send({
+    res.status(httpStats.ok).send({
       cached: true,
       data: value,
       msg: 'success get data'
     })
   } catch (e) {
     console.error(e);
-
   } finally {
     await quitClient()
   }
