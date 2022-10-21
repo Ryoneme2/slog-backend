5be452187398f6e2617f7e5e4c8294639f5394e5
Ryoneme2 <worknarair6@gmail.com>
2022-10-21 16:49:34 +0700

    setup init server

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import dotenv from 'dotenv';
+import express, { Express } from "express";
+import { rateLimit } from 'express-rate-limit';
+import cors from "cors";
+dotenv.config()
+
+const app: Express = express()
+
+// express server config
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(rateLimit({
+  windowMs: 10000,
+  max: 200,
+  message: "Too many requests from this IP, please try again"
+}))
+
+app.get('/testGet', (_, res) => {
+  res.status(200).send('Get success')
+})
+
+const PORT = process.env.PORT || 8080;
+console.timeEnd('Server started');
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+})
+
+export default app;
