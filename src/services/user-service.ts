c672cd317ec3e0003e8c99d9c0f012302de61a4d
Ryoneme2 <worknarair6@gmail.com>
2022-10-22 16:20:22 +0700

    implement : get one service

--- src/services/user-service.ts
+++ src/services/user-service.ts
@@ -6,9 +6,53 @@ import dotenv from 'dotenv';
 import storageClient from '@config/connectStorage';
 import { v4 } from 'uuid';
 
-// import { hashString, decodePassword } from '../libs/DecryptEncryptString';
-// import randAvatar from '../libs/randomAvatar';
+import { hashString, decodePassword } from '@util/DecryptEncryptString';
+import randAvatar from '@util/randomAvatar';
 
 dotenv.config();
 
 const prisma = new PrismaClient();
+
+const _addUser = async (username: string) => {
+  return
+}
+
+const _getOne = async (username: string, select = {}) => {
+  try {
+    const config: Prisma.UsersFindUniqueArgs = {
+      where: {
+        username,
+      },
+    };
+
+    if (Object.keys(select).length !== 0) config.select = select
+
+    const [userData, countDiary] = await prisma.$transaction([
+      prisma.users.findUnique(config),
+      prisma.diaries.count({
+        where: {
+          assignTo: username,
+        },
+      }),
+    ]);
+
+    return {
+      isOk: true,
+      data: { ...userData, countDiary },
+      msg: userData == null ? 'no error but user not found' : '',
+    };
+  } catch (e) {
+    return {
+      isOk: false,
+      data: null,
+      msg: 'Internal error',
+    };
+  } finally {
+    prisma.$disconnect();
+  }
+}
+
+export {
+  _addUser,
+  _getOne
+}
