38bb1b92c4a1e5bb37e021efe0903237b90df2df
Ryoneme2 <worknarair6@gmail.com>
2022-10-22 16:28:38 +0700

    implement : add user service

--- src/services/user-service.ts
+++ src/services/user-service.ts
@@ -13,8 +13,77 @@ dotenv.config();
 
 const prisma = new PrismaClient();
 
-const _addUser = async (username: string) => {
-  return
+const _addUser = async (data: {
+  firstName: string,
+  lastName: string,
+  email: string,
+  username: string,
+  password: string,
+}) => {
+  try {
+    const randString = v4();
+
+    const [hashedPassword, png] = await Promise.all([
+      hashString(data.password),
+      randAvatar(randString),
+    ]);
+    if (!hashedPassword) return {
+      isOk: false,
+      msg: 'hash password error'
+    }
+    const storageUrl =
+      'https://oijsgpmyxcrqexaewofb.supabase.co/storage/v1/object/public/';
+
+    const x = await storageClient
+      .from('dii-project-bucket')
+      .upload(`avatar/${randString}.png`, png, {
+        cacheControl: '3600',
+        upsert: false,
+      });
+
+    if (x.error) throw new Error(`${x.error.name} : ${x.error.message}`);
+
+    const imageUrl = `${storageUrl}${x.data.path}`;
+
+    await prisma.users.create({
+      data: {
+        fname: data.firstName,
+        lname: data.lastName,
+        email: data.email,
+        avatar: imageUrl,
+        username: data.username,
+        password: hashedPassword.hash,
+      },
+    });
+    return {
+      isOk: true,
+      msg: 'create success',
+    };
+  } catch (e) {
+    console.log(e);
+    if (e instanceof Prisma.PrismaClientKnownRequestError) {
+      // The .code property can be accessed in a type-safe manner
+      if (e.code === 'P2002') {
+        return {
+          success: false,
+          data: {},
+          msg: `username is already taken`,
+        };
+      }
+      return {
+        success: false,
+        data: {},
+      };
+    }
+
+    return {
+      success: false,
+      data: {},
+      msg: 'Internal Server Error Agent',
+    };
+  } finally {
+    prisma.$disconnect()
+  }
 }
 
 const _getOne = async (username: string, select = {}) => {
